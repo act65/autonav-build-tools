60b6d4430b80e08eb8260c57f4a19e9193966635
act65 <te1farac@gmail.com>
2020-05-26 14:49:47 +1200

    .

--- test/launch_docker.sh
+++ test/launch_docker.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 docker run -it \
-    --net host \ # may as well YOLO
-    --cpuset-cpus 0 \ # control the cpu
-    --memory 512mb \ # max memory it can use
-    -v /tmp/.X11-unix:/tmp/.X11-unix \ # mount the X11 socket
-    -e DISPLAY=unix$DISPLAY \ # pass the display
-    -v $HOME/Downloads:/root/Downloads \ # optional, but nice
-    -v $HOME/.config/google-chrome/:/data \ # if you want to save state
-    --device /dev/snd \ # so we have sound
-    --name chrome \
-    jess/chrome
+--net=host \
+-u telfaralex \
+--name test1 \
+--env="DISPLAY"  \
+--env="QT_X11_NO_MITSHM=1"  \
+--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+test:firefox
