5788fb29e92f0a12e0237fa1cd524a8e0dbb1e4f
act65 <te1farac@gmail.com>
2020-05-27 12:40:02 +1200

    .

--- test/local/launch_dockerfile.sh
+++ test/local/launch_dockerfile.sh
@@ -5,5 +5,5 @@ docker run -it \
 --env="DISPLAY"  \
 --env="QT_X11_NO_MITSHM=1"  \
 --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-test:xclock \
+test:local \
 xclock
---
+++ test/remote/launch_dockerfile.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+touch $XAUTH
+xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+
+docker run -it \
+        -e DISPLAY=$DISPLAY \
+        --volume=$XSOCK:$XSOCK:rw \
+        --volume=$XAUTH:$XAUTH:rw \
+        --env="XAUTHORITY=${XAUTH}" \
+        --user="telfaralex" \
+        test:remote \
+        xclock
