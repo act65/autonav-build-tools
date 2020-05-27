cab513de1b02bad358ad38a5b7f144a69dafb226
act65 <te1farac@gmail.com>
2020-05-27 12:34:08 +1200

    .

--- local/launch_docker.sh
+++ local/launch_docker.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-# xhost +local:docker
-
-docker run -it \
---env="DISPLAY=unix$DISPLAY" \
---volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-autonav:local
+xhost +local:docker
 
 # docker run -it \
-# --env="DISPLAY"  \
-# --env="QT_X11_NO_MITSHM=1"  \
-# --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-# autonav:local
+#--env="DISPLAY=unix$DISPLAY" \
+#--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+#autonav:local
+
+docker run -it \
+--env="DISPLAY=$DISPLAY"  \
+--env="QT_X11_NO_MITSHM=1"  \
+--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+autonav:local
