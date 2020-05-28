e7b95ec12363d6a94b8b6e0f0f0f14a1a4240466
act65 <te1farac@gmail.com>
2020-05-29 09:44:32 +1200

    fixes and latest

--- test/remote/launch_dockerfile.sh
+++ test/remote/launch_dockerfile.sh
@@ -5,6 +5,7 @@ touch $XAUTH
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
 
 docker run -it \
+        --net=host \
         -e DISPLAY=$DISPLAY \
         --volume=$XSOCK:$XSOCK:rw \
         --volume=$XAUTH:$XAUTH:rw \
